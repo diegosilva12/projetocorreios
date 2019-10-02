310cf30e1e6ad8e6a8ec3f026b82d61324d412f0
Diego <silva.diego.r09@gmail.com>
2019-10-01 21:27:41 -0300

    Update

---
+++ front-end-application/src/pages/sign-up/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap>
+      
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: 'SingUp',
+  data: () => ({})
+};
+</script>
---
+++ front-end-application/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../pages/login/'
+import SignUp from '../pages/sign-up/'
+
+Vue.use(Router)
+
+const router = new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'Login',
+            component: Login 
+        },
+        {
+            path: '/sign-up',
+            name: 'SingUp',
+            component: SignUp
+        }
+    ]
+})
+
+export default router;
